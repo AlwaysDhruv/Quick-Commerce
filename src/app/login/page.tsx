06ed64b75d9e4d79b7e418271c7b88f6aeca74f6
Dhruv Sonavane (ML Researcher - Learning) <sonavanebharat92@gmail.com>
2025-07-25 05:26:08 +0000

    ReferenceError: React is not defined     at LoginPage (https://6000-fire

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import React from 'react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
